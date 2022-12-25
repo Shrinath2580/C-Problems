f1875791a9c569bf5887bfafe6b59f0980eee032
Shrinath Mishra <shrinath_mishra@outlook.com>
2022-12-25 18:26:59 +0530

    Add files via upload

---
+++ cppintro.cpp
@@ -0,0 +1,39 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    
+    int a = 123;
+    cout<< a <<endl;
+    cout<< "Main ladka Hu" << endl;
+    
+    
+    int b = 5;
+    int c = 6;
+    
+    bool first = (b==c);
+    cout<< first <<endl;
+
+    bool second = (b>c);
+    cout<< second <<endl;
+
+    bool third = (b<c);
+    cout<< third <<endl;
+
+    bool fourth = (b<=c);
+    cout<< fourth <<endl;
+
+    bool fifth = (b>=c);
+    cout<< fifth <<endl;
+
+    bool sixth = (b!=c);
+    cout<< sixth <<endl; 
+    
+
+    int x = 5/2.0;
+    cout<< x << endl;
+    
+    cout << 2/5 <<endl;
+    
+}
