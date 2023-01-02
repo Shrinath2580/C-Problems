6630e8ca07e1b6dc18ccc451b5fe9026f9a347c8
Shrinath Mishra <shrinath_mishra@outlook.com>
2023-01-02 13:02:58 +0530

    Add files via upload

---
+++ BitwiseOperators.cpp
@@ -0,0 +1,88 @@
+#include<iostream>
+using namespace std;
+
+int main() {
+/*
+
+    int a=4;
+    int b=6;
+
+    cout<<" a&b "<< (a&b) <<endl;
+    cout<<" a|b "<< (a|b) <<endl;
+    cout<<" ~a "<< ~a <<endl;
+    cout<<" a^b "<< (a^b) <<endl;
+    
+
+    cout<< (17>>1) <<endl;
+    cout<< (17>>2) <<endl;
+    cout<< (19<<1) <<endl;
+    cout<< (21<<1) <<endl;
+
+    int i=7;
+
+    cout<< (++i) <<endl;
+    // o/p= 8 , i=8
+    cout<< (i++) <<endl;
+    // o/p= 8, i=9
+    cout<< (i--) <<endl;
+    // o/p= 9, i=8 
+    cout<< (--i) <<endl;
+    // o/p= 7, i=7
+
+
+    int a,b=1;
+    a=10;
+    if(++a)
+        cout<<b;
+    else
+        cout<<++b;
+
+
+
+    int a=1;
+    int b=2;
+
+    if(a-->0 && ++b>2) {
+        cout<<"Stage1 - Inside If ";
+    }
+    else{
+        "Stage2 - Inside else ";
+    }
+
+    cout<< a <<" " <<b<<endl;
+
+
+
+    int a=1;
+    int b=2;
+
+    if(a-->0 || ++b>2) {
+        cout<<"Stage1 - Inside If ";
+    }
+    else{
+        "Stage2 - Inside else ";
+    }
+
+    cout<< a <<" " <<b<<endl;
+
+
+
+    int number = 3;
+    cout<<(25*(++number));
+
+
+
+
+    int a=1;
+    int b=a++;
+    //b=1,a=2
+    int c=++a;
+    //a=3, c=3
+    cout<<b;
+    //1
+    cout<<c;
+    //3
+*/
+
+
+}
