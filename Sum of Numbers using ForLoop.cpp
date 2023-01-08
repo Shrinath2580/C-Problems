f29ccb7453f9531679e343c0497df4db27eecabf
Shrinath Mishra <shrinath_mishra@outlook.com>
2023-01-08 15:39:26 +0530

    Add files via upload

---
+++ Sum of Numbers using ForLoop.cpp
@@ -0,0 +1,16 @@
+//Sum of Numbersssss
+#include<iostream>
+using namespace std;
+
+int main() {
+
+    int n;
+    cout<<"Enter the value of n" <<endl;
+    cin>>n;
+    int sum=0;
+    for(int i=1;i<=n;i++) {
+        sum+=i;
+
+    }
+    cout<<sum<<endl;
+}
