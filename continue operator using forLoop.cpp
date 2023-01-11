6795e5879b30cbf305807fab0e58137ca53c9ee4
Shrinath Mishra <shrinath_mishra@outlook.com>
2023-01-11 20:20:03 +0530

    Add files via upload

---
+++ continue operator using forLoop.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+
+int main() {
+
+    int n;
+    cout<<"Enter the value of n" <<endl;
+    cin>>n;
+	
+	//Continue Operatorrrrrrrrrrr
+    for(int i=1; i<=n;i++) {
+        cout<<"Hi"<<endl;
+        cout<<"Hey"<<endl;
+        continue;
+        cout<<"Reply toh karde"<<endl;
+
+    }
+}
