9e15075d25d93fc704b18a378687977bed02a152
Shrinath Mishra <shrinath_mishra@outlook.com>
2023-04-16 12:05:59 +0530

    Add files via upload

---
+++ sort01.cpp
@@ -0,0 +1,38 @@
+#include<iostream>
+using namespace std;
+
+void PrintArray(int arr[], int n){
+    for(int i = 0; i<n; i++){
+        cout<<arr[i]<<" ";
+    }
+    cout<<endl;
+
+}
+
+void SortOne(int arr[], int n){
+    int left = 0; int right = n-1;
+
+    while(left<right){
+        while(arr[left] == 0 && left<right){
+            left++;
+        }
+
+        while(arr[right] == 1 && left < right){
+            right--;
+        }
+
+        if(left< right){
+            swap(arr[left],arr[right]);
+            left++;
+            right--;
+        }
+    }
+}
+
+int main(){
+    int arr[8] = {1,1,0,0,0,0,1,0};
+
+    SortOne(arr,8);
+    PrintArray(arr,8);
+
+}
