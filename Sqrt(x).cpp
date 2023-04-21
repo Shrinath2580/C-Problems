0216607aaf34a3b4072afaa6d6402f3ca15a7f88
Shrinath Mishra <shrinath_mishra@outlook.com>
2023-04-21 10:40:39 +0530

    Add files via upload

---
+++ Sqrt(x).cpp
@@ -0,0 +1,36 @@
+//Given a non-negative integer x, return the square root of x rounded down to the nearest integer. The returned integer should be non-negative as well.
+class Solution {
+public:
+
+    int binarySearch(int n){
+        int s = 0;
+        int e = n;
+        long long int mid = s + (e-s)/2;
+        long long int ans = 0;
+
+        while(s<=e){
+            long long int square = mid * mid;
+            if(square == n){
+                return mid;
+            }
+
+            if(square < n){
+                ans = mid;
+                s = mid + 1;
+                
+            }
+
+            else {
+               e = mid - 1;
+            }
+            mid = s + (e-s)/2;
+        }
+
+        return ans;
+    }
+
+    int mySqrt(int x) {
+        return binarySearch(x);
+        
+    }
+};
