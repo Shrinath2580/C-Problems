89a058c45159dee5f04be8f3481f22ee6d60e827
Shrinath Mishra <shrinath_mishra@outlook.com>
2023-04-25 09:59:27 +0530

    Add files via upload

---
+++ ValidPalindrome.cpp
@@ -0,0 +1,61 @@
+/*https://leetcode.com/problems/valid-palindrome/description/
+A phrase is a palindrome if, after converting all uppercase letters into lowercase letters and removing all non-alphanumeric characters, it reads the same forward and backward. Alphanumeric characters include letters and numbers.
+
+Given a string s, return true if it is a palindrome, or false otherwise.
+*/
+
+#include<bits/stdc++.h>
+using namespace std;
+
+class Solution {
+
+private:
+    bool valid(char ch){
+        if((ch>='a' && ch<='z') || (ch>='A' && ch<='Z') || (ch>='0' && ch<='9')){
+            return 1;
+        }
+        return 0;
+    }
+
+    char tolowercase(char ch){
+        if((ch>='a' && ch<='z') || (ch>='0' && ch<='9')){
+            return ch;
+        }
+        else{
+            char temp = ch - 'A' + 'a';
+            return temp;
+        }
+    }
+
+    bool checkPalindrome(string a){
+        int s = 0;
+        int e = a.length()-1;
+
+        while(s<=e){
+            if(a[s] != a[e])
+                return 0;
+            else{
+                s++;
+                e--;
+            }
+        }
+        return 1;
+    }
+
+public:
+    bool isPalindrome(string s) {
+        string temp = "";
+
+        for(int j = 0; j<s.length(); j++){
+            if(valid(s[j]))
+                temp.push_back(s[j]);
+        }
+
+        for (int j = 0; j<temp.length(); j++){
+            temp[j] = tolowercase(temp[j]);
+        }
+        return checkPalindrome(temp); 
+    }
+
+    
+};
