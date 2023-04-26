c530e67a255029401fec69dbac5a1f61cafa9f49
Shrinath Mishra <shrinath_mishra@outlook.com>
2023-04-26 08:44:37 +0530

    Add files via upload

---
+++ Maximum_Occuring_Character.cpp
@@ -0,0 +1,45 @@
+/*https://practice.geeksforgeeks.org/problems/maximum-occuring-character-1587115620/1*/
+
+#include<iostream>
+using namespace std;
+
+char getMaxiOccCharater(string s){
+    int arr[26] = {0};
+
+    for(int i = 0; i<s.length(); i++){
+        char ch = s[i];
+
+        int number = 0;
+        //lowercase
+        if(ch >= 'a' && ch <='z'){
+            number = ch - 'a';
+        }
+        else{
+            //uppercase
+            number = ch - 'A';
+        }
+        arr[number]++;
+    }
+
+    int maxi = -1, ans = 0;
+    for(int i = 0; i<26; i++){
+        if(maxi < arr[i]){
+            ans = i;
+            maxi = arr[i];
+        }
+    }
+
+    char finalAns = 'a' + ans;
+
+    return finalAns;
+}
+
+
+int main(){
+    string s;
+    cin>>s;
+
+    cout<<getMaxiOccCharater(s)<<endl;
+
+    return 0;
+}
