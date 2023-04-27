67da67981d50936351d0af9b09d803304c579410
Shrinath Mishra <shrinath_mishra@outlook.com>
2023-04-27 09:19:50 +0530

    Add files via upload

---
+++ String_compression.cpp
@@ -0,0 +1,53 @@
+/*https://leetcode.com/problems/string-compression/description/
+
+Given an array of characters chars, compress it using the following algorithm:
+
+Begin with an empty string s. For each group of consecutive repeating characters in chars:
+
+    If the group's length is 1, append the character to s.
+    Otherwise, append the character followed by the group's length.
+
+The compressed string s should not be returned separately, but instead, be stored in the input character array chars. Note that group lengths that are 10 or longer will be split into multiple characters in chars.
+
+After you are done modifying the input array, return the new length of the array.
+
+You must write an algorithm that uses only constant extra space.
+*/
+
+#include<bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    int compress(vector<char>& chars) {
+        int i = 0;
+        int ansIndex = 0;
+        int n = chars.size();
+        while(i < n){
+            int j = i+1;
+
+            while(j < n && chars[i] == chars[j]){
+                j++;
+            }
+            //yaha kab aaoge
+            //ya toh vector poora traverse krdia 
+            //ya fir new/different character encounter kia hai
+
+            //oldChar store karlo
+            chars[ansIndex++] = chars[i];
+
+            int count = j-i;
+
+            if(count>1){
+                //converting counting into single digit and saving in answer
+                string cnt = to_string(count);
+                for(char ch: cnt){
+                    chars[ansIndex++] = ch;
+                }
+            }
+            //moving to new/different character
+            i = j;
+        }
+        return ansIndex;
+    }
+};
