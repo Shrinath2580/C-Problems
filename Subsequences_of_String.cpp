666db65074b95a8823ef4ae4162566e4470d3417
Shrinath Mishra <shrinath_mishra@outlook.com>
2023-06-03 21:07:37 +0530

    Add files via upload

---
+++ Subsequences_of_String.cpp
@@ -0,0 +1,36 @@
+/*https://www.codingninjas.com/codestudio/problems/subsequences-of-string_985087?leftPanelTab=0&utm_source=youtube&utm_medium=affiliate&utm_campaign=love_babbar_16
+
+*/
+
+#include <bits/stdc++.h> 
+using namespace std;
+
+
+void solve(string str, string output, int index, vector<string>& ans){
+
+	//base case
+	if(index >= str.length()){
+		if(output.length() > 0){
+			ans.push_back(output);
+		}
+		return;
+	}
+
+	//exclude
+	solve(str, output, index+1, ans);
+
+	//include
+	char element = str[index];
+	output.push_back(element);
+	solve(str, output, index+1,  ans);
+}
+
+vector<string> subsequences(string str){
+	
+	vector<string> ans;
+	string output = "";
+	int index = 0;
+	solve(str, output, index, ans);
+	return ans;
+	
+}
