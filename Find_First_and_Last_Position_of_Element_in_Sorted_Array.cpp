084027ebd57e429c6e1ea8d3c929973cc9b113da
Shrinath Mishra <shrinath_mishra@outlook.com>
2023-06-28 10:46:07 +0530

    Add files via upload

---
+++ Find_First_and_Last_Position_of_Element_in_Sorted_Array.cpp
@@ -0,0 +1,34 @@
+/*
+https://leetcode.com/problems/find-first-and-last-position-of-element-in-sorted-array/
+
+Given an array of integers nums sorted in non-decreasing order, find the starting and ending position of a given target value.
+
+If target is not found in the array, return [-1, -1].
+
+You must write an algorithm with O(log n) runtime complexity.
+*/
+
+#include<bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    vector<int> searchRange(vector<int>& nums, int target) {
+        int startingPosition = -1, endingPosition = -1;
+        int n = nums.size();
+        for(int i = 0; i<n; i++){
+            if(nums[i] == target){
+                startingPosition = i;
+                break;
+            }
+        }
+        for(int i = n-1; i>=0; i--){
+            if(nums[i] == target){
+                endingPosition = i;
+                break;
+            }
+        }
+        return {startingPosition, endingPosition};
+    }
+};
+
